e198b9b143897fe87d6210feed63b05fa4781139
ARKDevelopment <111500885+ARKDevelopment@users.noreply.github.com>
2022-09-26 14:48:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ with sync_playwright() as p:
   try:
     browser = p.chromium.connect_over_cdp('http://localhost:9222')
   except:
-    os.system("xvfb-run -a chromium --remote-debugging-port=9222 --no-sandbox &")
-    sleep(5)
+    os.system("xvfb-run -a chromium --remote-debugging-port=9222 --no-sandbox --disable-gpu --disable-dev-shm-usage --disable-setuid-sandbox --disable-web-security --disable-features=IsolateOrigins,site-per-process &")
+    sleep(10)
     browser = p.chromium.connect_over_cdp('http://localhost:9222')
   page = browser.new_page()
   page.goto("https://wizzair.com/en-gb#/booking/select-flight/WAW/MAD/2022-10-17/null/1/0/0/null")
