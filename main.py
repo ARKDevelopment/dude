e776288378ffca7fb14fd11bd485af6dc7df5e74
ARKDevelopment <111500885+ARKDevelopment@users.noreply.github.com>
2022-09-26 15:04:22 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from playwright.sync_api import sync_playwright
 
 with sync_playwright() as p:
   try:
-    browser = p.chromium.connect_over_cdp('http://localhost:9222')
+    browser = p.chromium.connect_over_cdp('http://localhost:9220')
   except:
-    os.system("xvfb-run -a chromium --remote-debugging-port=9222 --no-sandbox --disable-gpu --disable-dev-shm-usage --disable-setuid-sandbox --disable-web-security --disable-features=IsolateOrigins,site-per-process &")
+    os.system("xvfb-run -a chromium --remote-debugging-port=9220 --no-sandbox --disable-gpu &")
     sleep(10)
-    browser = p.chromium.connect_over_cdp('http://localhost:9222')
+    browser = p.chromium.connect_over_cdp('http://localhost:9220')
   print("starting")
   page = browser.new_page()
   page.goto("https://wizzair.com/en-gb#/booking/select-flight/WAW/MAD/2022-10-17/null/1/0/0/null")
   print("waiting")
   page.click("button >> text=Allow all", timeout=0)
-  price = page.locator('[data-test="current-price"]').first.inner_text().replace("‎", "")
+  # input()
+  try:
+    print("format 1")
+    price = page.locator('[data-test="current-price"]').first.inner_text().replace("‎", "")
+  except:
+    print("format 2")
+    price = page.locator('[data-test="fare-type-button"] span').first.inner_text().replace("‎", "")
   open("result.txt", "w").write(price)
   print("done")
